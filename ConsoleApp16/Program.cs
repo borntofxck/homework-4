6f5c9d4fcdcf05012488bc70ca1c73cf618f3b9c
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-09-21 00:26:42 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int sum= a+b+c ; 
+            int mult= a*b*c ;
+            double average = (double)sum / 3;
+
+            Console.WriteLine($"Сумма:{sum}");
+            Console.WriteLine($"Произведение:{mult}");
+            Console.WriteLine($"Среднее арифметическое: {average:F3}");
+
+
+        }
+    }
+}
